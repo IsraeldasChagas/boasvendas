4466237fea20dc0133bb2bef25b4e235d0b90ee2
Israel Chagas <seuemail@gmail.com>
2026-05-12 18:54:08 -0400

    AtualizaÃ§Ã£o

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'A sua senha foi redefinida.',
+    'sent' => 'Enviámos por e-mail o link para redefinir a senha.',
+    'throttled' => 'Aguarde um momento antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => 'Não encontrámos nenhum utilizador com esse endereço de e-mail.',
+
+];
